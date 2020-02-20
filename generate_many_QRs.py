3a8293e96b444318322d482a7ac0ead063a55ea4
luucamay <lupemaydana@gmail.com>
2020-02-20 15:38:06 -0400

    removing not needed code

--- generate_many_QRs.py
+++ generate_many_QRs.py
@@ -9,8 +9,3 @@ for i in range(len(data)):
     url = pyqrcode.create(data[i])
     png_name = str(i) + ".text.png"
     url.png(png_name, scale = 8)
-'''
-s = "Capitulo 5: El codigo femenino \n http://hope.sulabatsu.com/2017/12/12/capitulo-5-el-codigo-femenino/"
-url = pyqrcode.create(s)
-url.png("5.hope.5.png", scale = 8)
-'''
